8d193baee0dfa1450391a18d82fc6b5a8799c5e1
ryanboodhoo <boodho46@gmail.com>
2024-09-18 23:59:29 -0400

    adding rectangle

---
+++ shapeProject-main/src/main/java/shapeProject/Rectangle.java
@@ -0,0 +1,16 @@
+package shapeProject;
+
+public class Rectangle extends Shape{
+
+    @Override
+    public double calculateArea() {
+        System.out.println("Area of Rectangle:");
+        return length * width;
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        System.out.println("Perimeter of Rectangle:");
+        return (length + width) * 2;
+    }
+}
---
+++ shapeProject-main/src/main/java/shapeProject/ShapeProject.java
@@ -0,0 +1,21 @@
+package shapeProject;
+
+public class ShapeProject {
+
+    public static void main(String[] args) {
+
+        Shape circle = new Circle(7);
+        circle.printArea();
+        circle.printPerimeter();
+
+        Rectangle rectangle = new Rectangle();
+        rectangle.length = 10;
+        rectangle.width = 10;
+        rectangle.printArea();
+
+
+        Rectangle rectangle2 = new Rectangle();
+        rectangle2.length = 10;
+        rectangle2.width = 10;
+        rectangle2.printPerimeter();    }
+}
