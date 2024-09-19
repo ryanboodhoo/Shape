419488cb8009037a49e90c8046c66972ac0f1633
ryanboodhoo <boodho46@gmail.com>
2024-09-18 21:30:38 -0400

    "shape class rectangle"

---
+++ src/main/java/Calc.java
@@ -0,0 +1,15 @@
+public class Calc {
+    public static void main(String[] args) {
+
+        Rectangle rectangle = new Rectangle();
+        rectangle.length = 10;
+        rectangle.width = 10;
+        rectangle.printArea();
+
+
+        Rectangle rectangle2 = new Rectangle();
+        rectangle2.length = 10;
+        rectangle2.width = 10;
+        rectangle2.printPerimeter();
+     }
+}
---
+++ src/main/java/Shape.java
@@ -0,0 +1,12 @@
+public class Shape {
+    public double length;
+    public double width;
+
+    public void printArea() {
+        System.out.println("Area of shape");
+    }
+
+    public void printPerimeter() {
+        System.out.println("Perimeter of shape");
+    }
+}
